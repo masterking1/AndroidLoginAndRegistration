38e5718766ffc43b881da77d95b42330cb1e7db0
Leyva <lleyva@deloittemx.com>
2017-03-22 12:27:04 -0600

    Initial commit

---
+++ app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
@@ -0,0 +1,9 @@
+package info.androidhive.loginandregistration.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://celyber.com/Deloitte/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://celyber.com/Deloitte/register.php";
+}
